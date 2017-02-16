95d14d64166c19ed3cd93af885c8a105093e10a7
Jonathan Cowling <jonathan.cowling@student.manchester.ac.uk>
2017-02-16 14:18:12 +0000

    justplayformresponse.php changed loop to use an array from a function rather than information from a db, i still need the give said function a location and a uuid

--- frontend/web-pages/justplayformresponse.php
+++ frontend/web-pages/justplayformresponse.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="author" content="Jonathan Cowling">
   <meta name="date" content="2 Feb 2017">
-  <meta name="date edited" content="13 Feb 2017">
+  <meta name="date edited" content="16 Feb 2017">
   <title>Just Play Form Response</title>
 </head>
 <body>
@@ -12,33 +12,18 @@
   <div id="mapContainer" style="width:100%;height:400px;"></div>
   
   <div id="rankedResponses">
+    
     <?php
-      
-      // make an inital query to find out the number of rows to include
-      $initialQuery = $mysqli->query("SELECT * FROM rankedRequests");
-      $responseLength = $initialQuery->num_rows;
-      $initialQuery->close();
-      
-      // loop to print out each match in order of rank
-      // assumes that the best rank is 1, and the worst is the number of ranks
-      for ($responseCount = 0 ; $responseCount < $responseLength;
-           $responseCount++)
+    
+      // an ordered array cointaining references to all matches
+      $rankedMatches = getrankedMatches(uerserID);
+    
+      foreach ($rankedMatches as $rankedMatch)
       {
-        // select a single match based from rank
-        $response = $mysqli->prepare("SELECT * FROM rankedRequests
-                                     WHERE rank=?");
-        $response->bind_param("i", $responseCount + 1);
-        $response->execute();
-        
-        // print out each result
-        $response->store_results();
-        $response->bind_result($result)
-      
-        echo $result;
-        
-        $response->close();
+       echo("<div>$rankedMatch</div>")
       }
-      
+      unset ($rankedMatch)
+    
     ?>
   </div>
 
