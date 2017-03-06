0f740281eaf84b4c4bc2d08f3d1b66edc0c6b15d
Melvin Sowah <melvin.sowah@student.manchester.ac.uk>
2017-03-06 17:48:46 +0000

    Debugging

--- lib/server.php
+++ lib/server.php
@@ -6,18 +6,18 @@
 // parameters in the URL, along with the connection, to the method that matches
 // a given route
 
-require_once("config.inc.php");
-require_once("../conf/routes.php");
+// require_once("config.inc.php");
+// require_once("../conf/routes.php");
 
-$router = compileRoutes();
+// $router = compileRoutes();
 
 echo $_SERVER["REQUEST_METHOD"]." ".$_SERVER["REQUEST_URI"]."\n";
 
-$res = $router->resolve($_SERVER["REQUEST_METHOD"], $_SERVER["REQUEST_URI"]);
+// $res = $router->resolve($_SERVER["REQUEST_METHOD"], $_SERVER["REQUEST_URI"]);
 
-foreach ($res as $key => $value) {
-  echo "Key: $key; Value: $value\n";
-}
+// foreach ($res as $key => $value) {
+//   echo "Key: $key; Value: $value\n";
+// }
 
 
 ?>
