d423492ceb18e2dda0145a6f0b8182a58a1fe177
Melvin Sowah <melvin.sowah@student.manchester.ac.uk>
2017-03-07 12:43:13 +0000

    routes.php: brackets

--- conf/routes.php
+++ conf/routes.php
@@ -3,13 +3,13 @@
 require_once("../lib/router.php");
 
 function compileRoutes() {
-  return new Router()
+  return (new Router()
     
     // Add new routes here. Do not terminate with a semi colon; that is
     // done at the end
     ->addRoute("GET", "/", "home.php")
 
-  ;
+  );
 }
 
 ?>
