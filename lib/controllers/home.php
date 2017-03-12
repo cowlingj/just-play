b31ba504533cea5d32227d7d534fccdc9c052bf9
Melvin Sowah <melvin.sowah@student.manchester.ac.uk>
2017-03-12 09:15:12 +0000

    Debuging

--- lib/controllers/home.php
+++ lib/controllers/home.php
@@ -1,12 +1,11 @@
 <?php
 
 function read($pathArgs, $queryArgs, $database) {
-  global $title, $text;
 
   $title = "Home | Just Play";
   $text = "Welcome!";
 
-  layout("home");
+  require layout("home");
 }
 
 ?>
