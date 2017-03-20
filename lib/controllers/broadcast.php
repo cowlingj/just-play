f8460ba034b9741be1102d66d42b96fc448e63cc
Jonathan Cowling <jonathan.cowling@student.manchester.ac.uk>
2017-03-20 12:11:37 +0000

    added a variable for a users broadcast in the broadcast controler

--- lib/controllers/broadcast.php
+++ lib/controllers/broadcast.php
@@ -1,6 +1,9 @@
 <?php 
   // display the broadcast form
   function read($path, $query, $db) {
+    
+    $correspondingBroadcast = $db-> query('SELECT * FROM broadcasts WHERE id = $_SESSION["userId"]')->fetch_assoc();
+
     require layout("broadcast-request-form");
   }
 
