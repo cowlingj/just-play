73ed6ca57f14e3fd3c58664f2e1aaa72aca03a19
Robbie <robert.mcvicker@student.manchester.ac.uk>
2017-03-22 11:53:44 +0000

    goog-login.php added

---
+++ lib/oauth/google-login.php
@@ -0,0 +1,13 @@
+<?php 
+
+  // Access the google api files
+  require_once '~/API/google-api-php-client2.1.1/vendor/autoupload.php';
+  
+  //Configure the client object
+  $client = new Google_Client();
+  $client->setAuthConfig('~/just-play/lib/oauth/client-secret-justplay.json');
+  $client->setAccessType("offline");
+  $client->setIncludeGrantedScopes(true);
+  $client->addScope(Google_Sign-In);
+  
+ ?>
