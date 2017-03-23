c0659ef41d6693efd8e3c5b5ae53e4deb1c0b906
Joel Biffin <Joel@Joels-MacBook-Pro.local>
2017-03-23 14:11:29 +0000

    Created route for accepting a request

---
+++ lib/controllers/request.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+// request?broadcastID=&recieverID=
+function destroy($path, $query, $db) {
+  // Delete record
+  $broadcastID = $query["broadcastID"];
+  $recieverID
+
+  $db->
+
+  // redirect to whatever route you need
+}
+
+
+
+
+?>
