1715591ae85bcdf4fdbcb8de1503c4a79632c4ac
Joel Biffin <Joel@Joels-MacBook-Pro.local>
2017-03-23 14:21:49 +0000

    request controller update

--- lib/controllers/request.php
+++ lib/controllers/request.php
@@ -4,11 +4,19 @@
 
 // request?broadcastID=&recieverID=
 function destroy($path, $query, $db) {
-  // Delete record
+  // the broadcast information from the query string
   $broadcastID = $query["broadcastID"];
-  $recieverID
+  $recieverID = $query["recieverID"];
 
-  $db->
+  // the broadcast information from the database
+  $broadcastInfo = $db->query("SELECT * FROM broadcast WHERE id='$broadcastID'");
+
+  // physically deleting the broadcast from the table
+  $db->query("DELETE * FROM broadcast WHERE id='$broadcastID'");
+
+
+  // creating the game from broadcast information
+  $db->query("INSERT INTO game (sport, ");
 
   // redirect to whatever route you need
 }
