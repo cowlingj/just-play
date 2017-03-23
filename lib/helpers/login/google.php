314770da58c501e2e81f98ede9be01896aeecd20
Robbie McVickers <robert.mcvickers@student.manchester.ac.uk>
2017-03-23 19:35:32 +0000

    google helper

---
+++ lib/helpers/login/google.php
@@ -0,0 +1,68 @@
+<?php 
+
+  require_once '/home/pi/Downloads/Google-API/vendor/autoload.php';
+
+  function init(){
+
+  setAuthConfig( '/home/pi/oauth/oauth/client_secret_justplay.json');
+  setRedirectUri('http://web.cs.manchester.ac.uk/mbax4msk/just_play/');
+  setScopes('email');
+    
+  } // init
+
+
+    public function isLoggedIn() {
+      return isset($_SESSION['access_token']);
+    }
+
+    public function getAuthUrl() {
+      return $this->client->createAuthUrl();
+    }
+
+    public function checkRedirectCode() {
+
+      if(isset($_GET['code'])) {
+
+        authenticate($_GET['code']);
+
+        setToken(setAccessToken());
+
+        storeUser(getPayload());
+
+        $payload = getPayload();
+
+        return true;
+      } // if 
+    } // checkRedirectCode
+
+      // get the payload  
+    public function getPayload() {
+      $token = verifyIdToken()->getAttributes()['payload'];
+      return $token; 
+    } // getPayload
+
+    public function setToken($token) {
+
+      $_SESSION['access_token'] = $token;
+      setAccessToken($token);
+
+    } // setToken
+
+    protected function storeUser($payload) {
+      $sql = "
+            INSERT INTO user (id, name, email)
+            VALUES ({$payload['id']}, '{$payload['name']}',
+                   '{$payload['email']}')
+            ON DUPLICATE KEY UPDATE id = id
+            ";
+      $this->db->query($sql);
+    }
+
+    public function logout() {
+      unset($_SESSION['access_token']);
+    } // logout
+
+
+  } // GoogleAuth
+
+ ?>
