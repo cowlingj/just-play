a5eedd235294334053814c28902f31efa59b81e1
Karan <karan.kapotra@student.manchester.ac.uk>
2017-03-23 20:04:54 +0000

    calling facebook functions

--- lib/controllers/login/facebook.php
+++ lib/controllers/login/facebook.php
@@ -4,7 +4,9 @@ require('lib/helpers/login/facebook.php');
 
 function read($pathArgs, $queryArgs, $database) {
 
+  exchangeToken();
   header("Location: /mbax4msk/just_play/search-form");
+
 }
 
 ?>
