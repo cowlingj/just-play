aeed275cfcfbe66cae8abd0d84f90d8933d0bec7
Joel Biffin <Joel@Joels-MacBook-Pro.local>
2017-03-23 19:25:54 +0000

    fixed routes.php

--- conf/routes.php
+++ conf/routes.php
@@ -17,12 +17,9 @@ function compileRoutes() {
   $router->addRoute("GET","/broadcast-request-form","broadcast");
   $router->addRoute("GET","/view-broadcast","view-broadcast");
   $router->addRoute("GET","/request","request");
-<<<<<<< HEAD
   $router->addRoute("GET", "/login-with-facebook","login/facebook");
   $router->addRoute("GET", "/view-accepted-broadcast", "view-accepted-broadcast");
-=======
   $router->addRoute("GET", "/login/facebook","login/facebook");
->>>>>>> ec5440ad7f4e3f1710e0d59520aae37d5acb70be
   return $router;
 }
 
