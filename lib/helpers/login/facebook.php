0b396de38d418a8c7ba1f1297e00621f6ec108cc
Karan <karan.kapotra@student.manchester.ac.uk>
2017-03-23 20:13:13 +0000

    sql things

--- lib/helpers/login/facebook.php
+++ lib/helpers/login/facebook.php
@@ -75,7 +75,7 @@ function getBasicInfo($fb) {
                 $id = $profile['id'];
                 $name = $profile['name'];
                 $email = $profile['email'];
-                $db ->query(INSERT INTO user (id, name, email) VALUES ($id, $name, $email); 
+                $db ->query("INSERT INTO user (id, name, email) VALUES ('$id', '$name', '$email')"); 
 	} catch(Facebook\Exceptions\FacebookResponseException $e) {
 		// When Graph returns an error
 		echo 'Graph returned an error: ' . $e->getMessage();
