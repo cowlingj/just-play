9ad3ccc6af36eb64e829ee5f033521d3c2d862db
Robbie McVickers <robert.mcvickers@student.manchester.ac.uk>
2017-03-24 10:08:03 +0000

    corrections to init

--- lib/helpers/google-oauth/app/init.php
+++ lib/helpers/google-oauth/app/init.php
@@ -3,9 +3,9 @@
   // Get the google api libary
   require_once 'lib/external/google-api/vendor/autoload.php';
   // Get the google class which contains the main functionality
-  require_once 'lib/helpers/google-ouath/app/classes/GoogleAuth.php';
+  require_once 'lib/helpers/google-oauth/app/classes/GoogleAuth.php';
   // Get the database class
-  require_once 'lib/helpers/google-ouath/app/app/classes/DB.php';
+  require_once 'lib/helpers/google-oauth/app/app/classes/DB.php';
 
 
  ?>
