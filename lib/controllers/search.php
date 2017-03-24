a4db14f42e45fbcbe47c0f2e4f032b24f9e7bd6c
Karan <karan.kapotra@student.manchester.ac.uk>
2017-03-24 11:00:55 +0000

    search.php

--- lib/controllers/search.php
+++ lib/controllers/search.php
@@ -1,9 +1,15 @@
 <?php
+require_once("lib/helpers/auth.php");
 function read($path, $query, $db) {
+
+  if(isLoggedIn())
+   $currentUser = getCurrentUser();
+  else 
+    header("Location: /mbax4msk/just_play");
 
   // db query will have 0 or 1 row depending on whether or not the user has a broadcast request
   // if it has a row, that row will be the users broadcast request
-  $correspondingBroadcast = $db->query("SELECT * FROM broadcast WHERE id = ".$_SESSION['userId']);
+  $correspondingBroadcast = $db->query("SELECT * FROM broadcast WHERE id = $currentUser['id']");
 
   // test whether the user has a broadcast request
   // if so view that request else view the search form
