3cc07943f8783b844a4b3e113b4c7dc958b1d42f
Joel Biffin <Joel@Joels-MacBook-Pro.local>
2017-03-24 14:55:37 +0000

    debug

--- lib/controllers/search.php
+++ lib/controllers/search.php
@@ -7,10 +7,7 @@ function read($path, $query, $db) {
 
     // retrieve reference to all sports in database
     $sports = $db->query("SELECT * FROM sport")->fetch_all(MYSQLI_ASSOC);
-
-    print_r($sports);
-    die("END");
-
+    
     // db query will have 0 or 1 row depending on whether or not the user has a broadcast request
     // if it has a row, that row will be the users broadcast request
     $correspondingBroadcast = $db->query("SELECT * FROM broadcast WHERE broadcaster='".$currentUser['id']."'");
