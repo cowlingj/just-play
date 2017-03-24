ab0874f49ed2e0632b7f3c606e1c4dd507f30d8e
Robbie McVickers <robert.mcvickers@student.manchester.ac.uk>
2017-03-24 13:25:31 +0000

    google

--- lib/controllers/login/google.php
+++ lib/controllers/login/google.php
@@ -8,16 +8,11 @@ function read($pathArgs, $queryArgs, $database) {
   $googleClient = new Google_Client();
   $db = databaseConnection();
   $googleAuth = new GoogleAuth($db, $googleClient);
-  $googleAuth->checkRedirectCode();
-  $payload = $googleAuth->getPayload();
-  print_r($payload);
-  die();
-
-  // Send to databse
-
-
-  header("Location: /mbax4msk/just_play/search-form");
-
+  if ($googleAuth->checkRedirectCode()) {
+    print_r($payload);
+    die();
+    header("Location: /mbax4msk/just_play/search-form");
+  } 
 }
 
 ?>
