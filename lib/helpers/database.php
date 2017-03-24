612fab932cc5a8153c68fe58a779fa53f7955762
Melvin Sowah <melvin.sowah@student.manchester.ac.uk>
2017-03-24 09:05:23 +0000

    Moved database functions out of index into database helper

---
+++ lib/helpers/database.php
@@ -0,0 +1,24 @@
+<?php
+
+  require_once("config.inc.php");
+
+  function databaseConnection($func = NULL) {
+    global $database_host, $database_user, $database_pass, $group_dbnames;
+    $connection = new mysqli(
+      $database_host,
+      $database_user,
+      $database_pass,
+      $group_dbnames[0]
+    );
+
+    if ($connection->connect_error)
+      die('Connect Error ('.$mysqli->connect_errno.') '.$mysqli->connect_error);
+    if ($func == NULL) return $connection;
+    else {
+      $result = $func($connection);
+      $connection->close();
+      return $result;
+    }
+  }
+
+?>
