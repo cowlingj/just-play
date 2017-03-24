1f62e466070648cac298b9f5668d577669fb66b3
Karan <karan.kapotra@student.manchester.ac.uk>
2017-03-24 11:58:20 +0000

    facebook stuff

--- lib/controllers/login/facebook.php
+++ lib/controllers/login/facebook.php
@@ -1,12 +1,15 @@
 <?php
 
 require('lib/helpers/login/facebook.php');
+require('lib/helpers/auth.php');
 
 function read($pathArgs, $queryArgs, $database) {
-
   exchangeToken();
-  header("Location: /mbax4msk/just_play/search-form");
+  if (!userExists("facebook", $id)) 
+    registerUser($name, $email, "facebook", $serviceID);
 
+  login("facebook", $id);
+  header("Location: /mbax4msk/just_play/search-form");
 }
 
 ?>
