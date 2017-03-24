f296e9952a87282b17cf7f6368276aca3a312883
Robbie McVickers <robert.mcvickers@student.manchester.ac.uk>
2017-03-24 12:25:40 +0000

    controllers google

--- lib/controllers/login/google.php
+++ lib/controllers/login/google.php
@@ -1,7 +1,7 @@
 <?php
 
 require('lib/helpers/login/google.php');
-require('lib/helpers/databse.php');
+require('lib/helpers/database.php');
 require('lib/helpers/auth.php');
 
 function read($pathArgs, $queryArgs, $database) {
@@ -13,7 +13,9 @@ function read($pathArgs, $queryArgs, $database) {
   $payload = $googleAuth->getPayload();
   print_r($payload);
   die();
-  // Send to databse 
+
+  // Send to databse
+
 
   header("Location: /mbax4msk/just_play/search-form");
 
