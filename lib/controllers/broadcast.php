5f4af1774969034507ef2781b8410462b2419916
Melvin Sowah <melvin.sowah@student.manchester.ac.uk>
2017-03-24 16:26:45 +0000

    Update broadcast.php

--- lib/controllers/broadcast.php
+++ lib/controllers/broadcast.php
@@ -9,7 +9,7 @@
     $correspondingBroadcast = $db-> query("SELECT * FROM broadcast WHERE id=".$_SESSION['userID'])->fetch_assoc();
 
     // the location of that broadcast
-    $correspondingBroadcastQuery = $db-> query("SELECT * FROM location WHERE id = $correspondingBroadcast['location']")->fetch_assoc();
+    $correspondingBroadcastQuery = $db-> query("SELECT * FROM location WHERE id=".$correspondingBroadcast['location'])->fetch_assoc();
     if ($correspondingBroadcast["reciever"] != null) { // THIS MAY CAUSE A NULL POINTER IF THE USER IS CREATIONG A BROADCAST REQUEST
       $accepted = true;
     }
