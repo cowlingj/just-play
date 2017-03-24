b53804b4c8830eeb264b328c176e20f1be08135d
Robbie McVickers <robert.mcvickers@student.manchester.ac.uk>
2017-03-24 11:09:29 +0000

    google.php

--- lib/controllers/login/google.php
+++ lib/controllers/login/google.php
@@ -1,13 +1,18 @@
 <?php
 
 require('lib/helpers/login/google.php');
+require('lib/helpers/databse.php');
+require('lib/helpers/auth.php');
 
 function read($pathArgs, $queryArgs, $database) {
 
   $googleClient = new Google_Client();
-  $db = new DB();
+  $db = databaseConnection();
   $googleAuth = new GoogleAuth($db, $googleClient);
   $googleAuth->checkRedirectCode();
+  $payload = $googleAuth->getPayload();
+  login('google', payload['']) 
+  // Send to databse 
 
   header("Location: /mbax4msk/just_play/search-form");
 
