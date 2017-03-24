1cd3c3a59f3e98f3e0b702d00cf8fad555d6407b
Karan Kapotra <karan.kapotra@student.manchester.ac.uk>
2017-03-24 14:46:50 +0000

    Update search.php

--- lib/controllers/search.php
+++ lib/controllers/search.php
@@ -7,13 +7,18 @@ function read($path, $query, $db) {
 
     // retrieve reference to all sports in database
     $sports = $db->query("SELECT * FROM sport")->fetch_all();
+
     // db query will have 0 or 1 row depending on whether or not the user has a broadcast request
     // if it has a row, that row will be the users broadcast request
-    $correspondingBroadcast = $db->query("SELECT * FROM broadcast WHERE id = " . $currentUser['id']);
-
+    $correspondingBroadcast = $db->query("SELECT * FROM broadcast WHERE broadcaster='".$currentUser['id']."'");
+    
+    if ($correspondingBroadcast) {
+      echo $db->error."<br>";
+      die("Failed to fetch broadcast");
+    }
     // test whether the user has a broadcast request
     // if so view that request else view the search form
-    if($correspondingBroadcast->num_rows() != 0) {
+    if($correspondingBroadcast->num_rows() > 0) {
       $correspondingBroadcast = $correspondingBroadcast->fetch_assoc();
       require layout("view-broadcast");
     }
