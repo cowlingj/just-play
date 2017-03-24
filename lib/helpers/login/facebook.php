bec65c1502648ba6813a14469ec2b84451f143e6
Karan <karan.kapotra@student.manchester.ac.uk>
2017-03-24 10:32:05 +0000

    added sql function

--- lib/helpers/login/facebook.php
+++ lib/helpers/login/facebook.php
@@ -1,5 +1,6 @@
 <?php
 require_once("lib/external/Facebook/autoload.php");
+require_once("lib/helpers/database.php");
 
 define(
   "FACEBOOK_REDIRECT_URI",
@@ -47,52 +48,59 @@ function exchangeToken(){
       echo "helper method helped... maybe";
     redirectHeader(); 
       echo "what ever this thing does";
-    getBasicInfo($fb, $db);
+    addFacebookDB();
       echo "i know what your name is ;)";
   } else header ("Location:https://web.cs.manchester.ac.uk/mbax4msk/just_play/");
 }
 
 function exchangeTokenHelper() {
   if (isset($_SESSION['facebook_access_token'])) {
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	} else {
-		// getting short-lived access token
-		$_SESSION['facebook_access_token'] = (string) $accessToken;
-	  	// OAuth 2.0 client handler
-		$oAuth2Client = $fb->getOAuth2Client();
-		// Exchanges a short-lived access token for a long-lived one
-		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-		// setting default access token to be used in script
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	}
+    $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+  } else {
+    // getting short-lived access token
+    $_SESSION['facebook_access_token'] = (string) $accessToken;
+      // OAuth 2.0 client handler
+    $oAuth2Client = $fb->getOAuth2Client();
+    // Exchanges a short-lived access token for a long-lived one
+    $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+    $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+    // setting default access token to be used in script
+    $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+  }
 }
 
 function redirectHeader() {
   if (isset($_GET['code'])) {
-		header('Location: mbax4msk/just_play/public/layouts/search');
-	}
+    header('Location: mbax4msk/just_play/public/layouts/search');
+  }
 }
 
-function getBasicInfo($fb, $db) {
-  	try {
-		$profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
-		$profile = $profile_request->getGraphNode()->asArray();
+function addFacebookDB() {
+  $db = databaseConnection();
+  $profile = getBasicInfo ($fb);
     $id = $profile['id'];
     $name = $profile['name'];
     $email = $profile['email'];
     $db ->query("INSERT INTO user (id, name, email) VALUES ($id, $name,  $email)"); 
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		// When Graph returns an error
-		echo 'Graph returned an error: ' . $e->getMessage();
-		session_destroy();
-		// redirecting user back to app login page
-		header("Location: /public/layouts/search.php");
-		exit;
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		// When validation fails or other local issues
-		echo 'Facebook SDK returned an error: ' . $e->getMessage();
-		exit;
-	}
+    $db->close();
+    return $userToDB;
+}
+
+function getBasicInfo($fb) {
+    try {
+    $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+    return $profile_request->getGraphNode()->asArray();
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    session_destroy();
+    // redirecting user back to app login page
+    header("Location: /public/layouts/search.php");
+    exit;
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+  }
 }
 ?>
